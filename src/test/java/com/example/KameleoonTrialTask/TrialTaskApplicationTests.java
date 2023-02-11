d44af14a10c361f2eb7f56d158bf4bb84700b721
Fedor Danilenko <divine.world.mail@gmail.com>
2023-02-11 13:35:10 +0300

    Create project

---
+++ src/test/java/com/example/KameleoonTrialTask/TrialTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KameleoonTrialTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
