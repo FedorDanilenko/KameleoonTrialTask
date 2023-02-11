990209dba7c5388c030c37bd48584cff84f61336
Fedor Danilenko <divine.world.mail@gmail.com>
2023-02-11 17:36:12 +0300

    Update "UserMapper" and create "UserService"

--- src/main/java/com/example/KameleoonTrialTask/mapper/UserMapper.java
+++ src/main/java/com/example/KameleoonTrialTask/mapper/UserMapper.java
@@ -4,12 +4,14 @@ import com.example.KameleoonTrialTask.dto.user.UserInDto;
 import com.example.KameleoonTrialTask.dto.user.UserOutDto;
 import com.example.KameleoonTrialTask.entity.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "dataCreated", expression = "java(java.time.LocalDateTime.now())")
     UserEntity toUser(UserInDto userInDto);
 
     UserOutDto toDto(UserEntity userEntity);
--- src/main/java/com/example/KameleoonTrialTask/service/UserService.java
+++ src/main/java/com/example/KameleoonTrialTask/service/UserService.java
@@ -1,12 +1,34 @@
 package com.example.KameleoonTrialTask.service;
 
+import com.example.KameleoonTrialTask.dto.user.UserInDto;
+import com.example.KameleoonTrialTask.dto.user.UserOutDto;
+import com.example.KameleoonTrialTask.exception.UserAlreadyExistEx;
+import com.example.KameleoonTrialTask.mapper.UserMapper;
 import com.example.KameleoonTrialTask.repository.UserRepo;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@Service
+@Data
 public class UserService {
 
     @Autowired
     private UserRepo userRepo;
+    private UserMapper userMapper;
+
+    private UserOutDto create(UserInDto userInDto) throws UserAlreadyExistEx {
+        try {
+            return userMapper.toDto(userRepo.save(userMapper.toUser(userInDto)));
+        } catch (DataAccessException ex) {
+            throw new UserAlreadyExistEx("User with this name already exists");
+        }
+    }
 
 
 }
