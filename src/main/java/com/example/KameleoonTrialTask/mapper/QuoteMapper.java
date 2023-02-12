da890b4a982f8a23189c31fd25c9fdcd114cd138
Fedor Danilenko <divine.world.mail@gmail.com>
2023-02-12 19:59:00 +0300

    update "QuoteOutDto"

---
+++ src/main/java/com/example/KameleoonTrialTask/mapper/QuoteMapper.java
@@ -0,0 +1,22 @@
+package com.example.KameleoonTrialTask.mapper;
+
+import com.example.KameleoonTrialTask.dto.quote.QuoteInDto;
+import com.example.KameleoonTrialTask.dto.quote.QuoteOutDto;
+import com.example.KameleoonTrialTask.dto.user.UserInDto;
+import com.example.KameleoonTrialTask.dto.user.UserOutDto;
+import com.example.KameleoonTrialTask.entity.QuoteEntity;
+import com.example.KameleoonTrialTask.entity.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface QuoteMapper {
+
+    QuoteMapper INSTANCE = Mappers.getMapper(QuoteMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    QuoteEntity toQuote(QuoteInDto quoteInDto);
+
+    QuoteOutDto toDto(QuoteOutDto quoteOutDto);
+}
