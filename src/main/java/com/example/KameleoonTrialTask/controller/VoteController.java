9ff2b6ed7714d2b59b78e181a8ce153abbfba6b7
Fedor Danilenko <divine.world.mail@gmail.com>
2023-02-13 21:09:08 +0300

    update VoteController

--- src/main/java/com/example/KameleoonTrialTask/controller/VoteController.java
+++ src/main/java/com/example/KameleoonTrialTask/controller/VoteController.java
@@ -1,10 +1,14 @@
 package com.example.KameleoonTrialTask.controller;
 
+import com.example.KameleoonTrialTask.exception.NotFoundEx;
 import com.example.KameleoonTrialTask.service.VoteService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/votes")
@@ -13,4 +17,20 @@ public class VoteController {
 
     @Autowired
     private VoteService voteService;
+
+    @PostMapping
+    public void voteUp (@PathVariable Long quouteId, @RequestBody Long userId) throws NotFoundEx {
+        voteService.voteUp(quouteId, userId);
+    }
+
+    @PostMapping
+    public void voteDown (@PathVariable Long quouteId, @RequestBody Long userId) throws NotFoundEx {
+        voteService.voteDown(quouteId, userId);
+    }
+
+    @GetMapping
+    public Map<Timestamp, Integer> getVoteHistory (@PathVariable Long quoteId) {
+        return voteService.getVoteHistory(quoteId);
+    }
+
 }
