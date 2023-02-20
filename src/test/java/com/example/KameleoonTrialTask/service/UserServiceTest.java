cb3ad9a297f4fb84a4c298425cb933b420caeddd
Fedor Danilenko <divine.world.mail@gmail.com>
2023-02-20 19:39:44 +0300

    Add SpringBootTest for service method create

---
+++ src/test/java/com/example/KameleoonTrialTask/service/UserServiceTest.java
@@ -0,0 +1,48 @@
+package com.example.KameleoonTrialTask.service;
+
+import com.example.KameleoonTrialTask.dto.user.UserInDto;
+import com.example.KameleoonTrialTask.dto.user.UserOutDto;
+import com.example.KameleoonTrialTask.exception.AlreadyExistEx;
+import com.example.KameleoonTrialTask.mapper.UserMapper;
+import com.example.KameleoonTrialTask.repository.UserRepo;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class UserServiceTest {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void create() throws AlreadyExistEx {
+        UserInDto userInDto = UserInDto.builder()
+                .name("login")
+                .email("email@email.com")
+                .password("pass")
+                .build();
+
+        UserOutDto userOutDto = userService.create(userInDto);
+
+        assertNotNull(userOutDto.getId());
+        assertEquals("login", userOutDto.getName());
+    }
+
+    @Test
+    void alreadyEx() throws AlreadyExistEx {
+
+    }
+}
